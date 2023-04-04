27b3df09b477c439c9e37532324434977d4882d5
Dennis Witt <witt.dennis@outlook.com>
2023-04-04 20:56:03 +0200

    add kubelet firewall rule for worker nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,6 +113,17 @@ resource "hcloud_firewall" "firewall_worker" {
       "::/0"
     ]
   }
+
+  rule {
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "10250"
+    description = "kubelet-api"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
 }
 
 resource "hcloud_firewall" "firewall_master" {
