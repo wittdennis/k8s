a4f0713bc8d2094d6851968f0e4de30fb0b9976d
Dennis Witt <witt.dennis@outlook.com>
2023-04-04 21:08:37 +0200

    add nodeport to worker firewall

--- terraform/main.tf
+++ terraform/main.tf
@@ -124,6 +124,17 @@ resource "hcloud_firewall" "firewall_worker" {
       "::/0"
     ]
   }
+
+  rule {
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "30000-32767"
+    description = "nodeport"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
 }
 
 resource "hcloud_firewall" "firewall_master" {
